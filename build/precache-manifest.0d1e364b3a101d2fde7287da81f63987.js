3e9d50af4212e762a9596654f51fc1f803805e02
karthik <karthikks880@gmail.com>
2020-04-28 22:08:16 +0530

    Add build commit

---
+++ build/precache-manifest.0d1e364b3a101d2fde7287da81f63987.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74d9582e8798a07a16903ebfb83b922",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b33361c35d54e5188678",
+    "url": "/static/css/main.cc22fbcc.chunk.css"
+  },
+  {
+    "revision": "9a6c894ec618f51668d0",
+    "url": "/static/js/2.7feece4e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.7feece4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33361c35d54e5188678",
+    "url": "/static/js/main.099a234f.chunk.js"
+  },
+  {
+    "revision": "ba8f440c0d175f428f5e",
+    "url": "/static/js/runtime-main.2f6061ad.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
